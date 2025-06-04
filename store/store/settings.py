812c9b94264e4f179b81a1618d17e35af1c1165c
tymoneq <50627886+tymoneq@users.noreply.github.com>
2025-06-04 18:13:02 +0200

    set settings

--- store/store/settings.py
+++ store/store/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1p#ivl&e!oa$1u10esggjh3hj&qxoual6ls%ls4x1679my-idg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['Tymoneq12345.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
